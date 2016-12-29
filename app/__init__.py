388749d12e1e1bf4107a023cff2223a44843760f
lefkou <elekousis@gmail.com>
2016-12-29 22:24:57 +0000

    Added sample html+css+structure

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/search", methods = ["POST"])
+def search():
+    return render_template("results.html")
