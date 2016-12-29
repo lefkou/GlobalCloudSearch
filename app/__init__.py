1c523e0e51f2c59c46b16943ca108ca088c4c4a4
lefkou <elekousis@gmail.com>
2016-12-29 23:04:04 +0000

    Added functionality for search through searchbar for dropbox

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
+import dropbox
 
 app = Flask(__name__)
 
+app_key = '7l8ffoj7zzz8p1j'
+app_secret = '8w15ijlf5037y6u'
+access_token = '4K6vAJC9MA4AAAAAAAACAO77hjgpiwlHMCqyWJ0mxwZvjKaiUu7nq7sg0A76gwT3'
+
+flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 
 @app.route("/")
 def hello():
@@ -10,4 +16,8 @@ def hello():
 
 @app.route("/search", methods = ["POST"])
 def search():
-    return render_template("results.html")
+    authorize_url = flow.start()
+    client = dropbox.client.DropboxClient(access_token)
+    file_name = request.form['search-input']
+    data =  client.search("", file_name, file_limit=1000, include_deleted=False)
+    return render_template("results.html", data = data[0]['path'])
